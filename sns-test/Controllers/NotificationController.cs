e65cfff035951baea95862b7f02d84a81c6067c7
rpozuelo20 <rubenpozuelomartinez@gmail.com>
2022-05-23 12:33:44 +0200

    Add project files.

---
+++ sns-test/Controllers/NotificationController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sns_test.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sns_test.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private ServiceAWSSNS _service;
+        public NotificationController(ServiceAWSSNS service)
+        {
+            this._service = service;
+        }
+
+        public IActionResult SendSNS()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendSNS(string num)
+        {
+            Random random = new Random();
+            int code = random.Next(100000, 999999);
+            TempData["CODE"] = code;
+            await this._service.EnviarSNS(num, code);
+            return RedirectToAction("VerifySNS");
+        }
+        public IActionResult VerifySNS()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult VerifySNS(int code)
+        {
+            int codigo = (int)TempData["CODE"];
+            if (code == codigo)
+            {
+                ViewData["MENSAJE"] = "VERIFICACION EXITOSA";
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "VERIFICACION FALLIDA";
+            }
+            return View();
+        }
+    }
+}
---
+++ sns-test/Services/ServiceAWSSNS.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sns_test.Services
+{
+    public class ServiceAWSSNS
+    {
+        private readonly IConfiguration _configuration;
+        public ServiceAWSSNS(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public async Task EnviarSNS(string num, int code)
+        {
+            //  Recuperamos las credenciales de nuestro usuario aws
+            string awsKeyId = this._configuration.GetConnectionString("awsKeyId").ToString();
+            string awsKeySecret = this._configuration.GetConnectionString("awsKeySecret").ToString();
+            var awsCredentials = new BasicAWSCredentials(awsKeyId, awsKeySecret);
+            //  Creamos el cliente sns
+            AmazonSimpleNotificationServiceClient snsClient = new AmazonSimpleNotificationServiceClient(awsCredentials, Amazon.RegionEndpoint.USEast1);
+            //  Creamos la peticion con los datos necesarios y publicamos la peticion
+            PublishRequest pubReq = new PublishRequest();
+            pubReq.Message = "Your code is "+code.ToString();
+            pubReq.PhoneNumber = num;
+            pubReq.MessageAttributes.Add("AWS.SNS.SMS.SMSType", new MessageAttributeValue { StringValue = "Transactional", DataType = "String" });
+            PublishResponse pubRes = await snsClient.PublishAsync(pubReq);
+        }
+    }
+}
